dbc8ceb0cffecf4641daab1b3f146e1a21c5918b
James Bampton <bamptini@hotmail.com>
2025-06-29 13:51:13 +0100

    Added class to the li element and id to the input element (checkbox) so I can look to use teh delete and edit functions to locate the list item to delete by the unique id

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -2,68 +2,12 @@
 
 //Home To do list array
 var homeArray = [
-
-
 ];
-
-/* var homeArray = [
-{
-    task: "Ironing",
-},
-
- {
-    task: "Repair door", 
-},
-
- {
-    task: "Cut Grass",
-},
-
- {
-    task: "Feed Lion",
-},
-
- {
-    task: "Tame Shrew",
-},
-
-
-]; */
 
 //WORK To do list array
 var workArray = [
-/* {
-    task: "Delete all files",
-},
-
- {
-    task: "Add bug to code",
- },
-
- {
-    task: "Unleash a virus",
-},
-
- {
-    task: "Send rude email to manager",
-}, */
 ];
 
 //SOCIAL To do list array
 var socialArray = [
-/* {
-    task: "PIL gig 4th July",
-},
-
- {
-    task: "Perform in charity event",
- },
-
- {
-    task: "Meet friedns for coffee",
-},
-
- {
-    task: "Visit Brighton for eats",
-}, */
 ];
